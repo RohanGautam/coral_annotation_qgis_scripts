7f7a567ca7f3cbe9f5efdb5a2e83433cc08cb376
Rohan Gautam <rohan1gautam@gmail.com>
2023-09-07 18:09:39 +0800

    add (redundant) inp so that menu is shown

--- save_current_layers.py
+++ save_current_layers.py
@@ -8,42 +8,46 @@ from qgis.processing import alg
 
 @alg(
     name="savecorallayersalg",
-    label="Save Vector layers",
+    label="Save vector layers",
     group="coralannotation",
     group_label="Coral annotation",
 )
-@alg.input(type=alg.BOOL, name="run", label="dummy", default=True)
-@alg.output(type=alg.NUMBER, name="indicator", label="Success indicator")
+@alg.input(type=alg.BOOL, name="run", label="Save vector layers", default=True)
+@alg.output(type=alg.BOOL, name="indicator", label="Success indicator")
 def save_layers_alg(instance, parameters, context, feedback, inputs):
     """
     Saves vector layers to an annotations directory.
     """
+    to_save = instance.parameterAsBool(parameters, "run", context)
     feedback.pushInfo("here")
 
-    # path of QGIS project file
-    project_file_path = Path(QgsProject.instance().absoluteFilePath())
-    project_directory_path = project_file_path.parent
+    if to_save:
+        # path of QGIS project file
+        project_file_path = Path(QgsProject.instance().absoluteFilePath())
+        project_directory_path = project_file_path.parent
 
-    feedback.pushInfo(f"Project root directory: {str(project_directory_path)}")
-    # create annotations directory if needed
-    annotations_dir_path = project_directory_path.joinpath("annotations")
-    if not os.path.exists(annotations_dir_path):
-        os.mkdir(annotations_dir_path)
+        feedback.pushInfo(f"Project root directory: {str(project_directory_path)}")
+        # create annotations directory if needed
+        annotations_dir_path = project_directory_path.joinpath("annotations")
+        if not os.path.exists(annotations_dir_path):
+            os.mkdir(annotations_dir_path)
 
-    # Get current layers
-    layers = list(QgsProject.instance().mapLayers().values())
+        # Get current layers
+        layers = list(QgsProject.instance().mapLayers().values())
 
-    # save vector layers
-    for l in layers:
-        if l.type() == QgsMapLayer.VectorLayer:
-            layer_name = l.name()
-            path_to_save = str(annotations_dir_path.joinpath(f"{layer_name}.shp"))
-            # save as shapefile
-            _writer = QgsVectorFileWriter.writeAsVectorFormat(
-                l, path_to_save, "utf-8", driverName="ESRI Shapefile"
-            )
-            # update data source of current layer to be from memory to file contents
-            l.setDataSource(
-                path_to_save + f"|layername={layer_name}", layer_name, "ogr"
-            )
-    return {"output": 1}
+        # save vector layers
+        for l in layers:
+            if l.type() == QgsMapLayer.VectorLayer:
+                layer_name = l.name()
+                path_to_save = str(annotations_dir_path.joinpath(f"{layer_name}.shp"))
+                # save as shapefile
+                _writer = QgsVectorFileWriter.writeAsVectorFormat(
+                    l, path_to_save, "utf-8", driverName="ESRI Shapefile"
+                )
+                # update data source of current layer to be from memory to file contents
+                l.setDataSource(
+                    path_to_save + f"|layername={layer_name}", layer_name, "ogr"
+                )
+        return {"output": True}
+    else:
+        return {"output": False}
